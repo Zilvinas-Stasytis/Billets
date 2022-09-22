7cad9d0cd55d1649c03bed30a37cf0bad6093a44
Zilvinas <107237017+Zilvinas-Stasytis@users.noreply.github.com>
2022-09-22 15:51:32 +0300

    Add files via upload

--- Billets/Program.cs
+++ Billets/Program.cs
@@ -1,78 +1,133 @@
-ï»¿
+/******************************************************************************
 
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
 
-int[] bilietai = { 0, 0, 0 };
-int menuItem;
-/*static OrderingTickets;
-static SoldingTickets;
-static Reportas;
-*/
-Console.WriteLine("Bilietai online ir ne tik");
-    Console.WriteLine("M E N I U");
-    Console.WriteLine("---------------------------------");
-    Console.WriteLine("1. BilietÅ³ uÅ¾sakymas");
-    Console.WriteLine("2. BilietÅ³ pardavimas");
-    Console.WriteLine("3. Ataskaita");
-    Console.WriteLine("4. Pabaiga");
-    Console.WriteLine("JÅ«sÅ³ pasirinkimas:");
-    menuItem = Convert.ToInt32(Console.ReadLine());
-    switch (menuItem)
-    {
-        case 1:
-            OrderingTickets(bilietai);
-            break;
-        /*case 2:
-            SoldingTickets(bilietai);
-            break;
-        case 3:
-            Reportas(bilietai);
-            break;*/
-    }
-Console.WriteLine("Bilietai po 10â‚¬ {0}, bilietai po 20â‚¬ {1}, bilietai po 30â‚¬ {2}", bilietai[0], bilietai[1], bilietai[2]);
- int OrderingTickets (int[] tickets)
+*******************************************************************************/
+
+/* Online C# Compiler and Editor */
+using System.IO;
+using System;
+public class Program
 {
-    int chooseItem; int kiekis;
+  static void Main ()
+  {
+    int[] bilietai = { 0, 0, 0 };
+    int menuItem;
     do
-    {
-        Console.WriteLine("Kokius bilietus uÅ¾sakysite?");
-        Console.WriteLine("---------------------------------");
-        Console.WriteLine("1. Kaina 10â‚¬");
-        Console.WriteLine("2. Kaina 20â‚¬");
-        Console.WriteLine("3. Kaina 30â‚¬");
-        Console.WriteLine("4. Atsisakysite uÅ¾sakymo");
-        chooseItem = Convert.ToInt32(Console.ReadLine());
-        switch (chooseItem)
-        {
-            case 1:
-                Console.WriteLine("Ä®veskite kiekÄ¯");
-                kiekis = Convert.ToInt32(Console.ReadLine());
-                tickets[0] += kiekis;
-                kiekis = 0;
-                break;
-            case 2:
-                Console.WriteLine("Ä®veskite kiekÄ¯");
-                kiekis = Convert.ToInt32(Console.ReadLine());
-                tickets[1] += kiekis;
-                kiekis = 0;
-                break;
-            case 3:
-                Console.WriteLine("Ä®veskite kiekÄ¯");
-                kiekis = Convert.ToInt32(Console.ReadLine());
-                tickets[2] += kiekis;
-                kiekis = 0;
-                break;
-        }
-    } while (chooseItem != 4);
-    
-    foreach (int v in tickets)
-    {
-        return v;
-    }
-    
-    /*static void SoldingTickets(int[] tickets) { 
-    };
-    static void Reportas(int[] tickets) { 
-    };*/
-}
+      {
+	Console.WriteLine ("Bilietai online ir ne tik");
+	Console.WriteLine ("M E N I U");
+	Console.WriteLine ("---------------------------------");
+	Console.WriteLine ("1. Bilietø uþsakymas");
+	Console.WriteLine ("2. Bilietø pardavimas");
+	Console.WriteLine ("3. Ataskaita");
+	Console.WriteLine ("4. Pabaiga");
+	Console.WriteLine ("Jûsø pasirinkimas:");
+	menuItem = Convert.ToInt32 (Console.ReadLine ());
+	switch (menuItem)
+	  {
+	  case 1:
+	    OrderingTickets (bilietai);
+	    break;
+	  case 2:
+	    SoldingTickets (bilietai);
+	    break;
+	    /*
+	       case 3:
+	       Reportas (bilietai);
+	       break; */
+	  }
+      }
+    while (menuItem != 4);
+    for (int i = 0; i < bilietai.Length; i++)
+      {
+	Console.WriteLine (bilietai[i]);
+      }
+  }
 
-       
+  static int[] OrderingTickets (int[]tickets)
+  {
+    int chooseItem;
+    int kiekis;
+    do
+      {
+	Console.WriteLine ("Kokius bilietus uþsakysite?");
+	Console.WriteLine ("---------------------------------");
+	Console.WriteLine ("1. Kaina 10€,");
+	Console.WriteLine ("2. Kaina 20€,");
+	Console.WriteLine ("3. Kaina 30€,");
+	Console.WriteLine ("4. Atsisakysite uþsakymo");
+	chooseItem = Convert.ToInt32 (Console.ReadLine ());
+	switch (chooseItem)
+	  {
+	  case 1:
+	    Console.WriteLine ("Áveskite kieká");
+	    kiekis = Convert.ToInt32 (Console.ReadLine ());
+	    tickets[0] += kiekis;
+	    kiekis = 0;
+	    break;
+	  case 2:
+	    Console.WriteLine ("Áveskite kieká");
+	    kiekis = Convert.ToInt32 (Console.ReadLine ());
+	    tickets[1] += kiekis;
+	    kiekis = 0;
+	    break;
+	  case 3:
+	    Console.WriteLine ("Áveskite kieká");
+	    kiekis = Convert.ToInt32 (Console.ReadLine ());
+	    tickets[2] += kiekis;
+	    kiekis = 0;
+	    break;
+	  }
+      }
+    while (chooseItem != 4);
+
+    return tickets;
+  }
+  static int[] SoldingTickets (int[]tickets)
+  {
+    int chooseItem;
+    int kiekis;
+    do
+      {
+	Console.WriteLine ("Kokius bilietus pirksite?");
+	Console.WriteLine ("---------------------------------");
+	Console.WriteLine ("1. Kaina 10€,");
+	Console.WriteLine ("2. Kaina 20€,");
+	Console.WriteLine ("3. Kaina 30€,");
+	Console.WriteLine ("4. Atsisakysite pirkimo");
+	chooseItem = Convert.ToInt32 (Console.ReadLine ());
+	switch (chooseItem)
+	  {
+	  case 1:
+	    Console.WriteLine ("Áveskite kieká");
+	    kiekis = Convert.ToInt32 (Console.ReadLine ());
+	    tickets[0] -= kiekis;
+	    kiekis = 0;
+	    break;
+	  case 2:
+	    Console.WriteLine ("Áveskite kieká");
+	    kiekis = Convert.ToInt32 (Console.ReadLine ());
+	    tickets[1] -= kiekis;
+	    kiekis = 0;
+	    break;
+	  case 3:
+	    Console.WriteLine ("Áveskite kieká");
+	    kiekis = Convert.ToInt32 (Console.ReadLine ());
+	    tickets[2] -= kiekis;
+	    kiekis = 0;
+	    break;
+	  }
+      }
+    while (chooseItem != 4);
+
+    return tickets;
+
+  }
+/*
+static Reportas(int[] tickets) {
+    return (int) tickets;
+}*/
+}
