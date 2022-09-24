4f298405e94a3b7da05d2a75528eab75dd871c81
Zilvinas -Stasytis <zalciukas@gmail.com>
2022-09-24 16:07:45 +0300

    Bilietai

--- Billets/Program.cs
+++ Billets/Program.cs
@@ -1,5 +1,5 @@
 ﻿int[] bilietai = { 0, 0, 0 };
-
+int[] uzsakyta = { 0, 0, 0 };
     int menuItem;
     do
       {
@@ -15,25 +15,18 @@
 	switch (menuItem)
 	  {
 	  case 1:
-	    OrderingTickets (bilietai);
+	    OrderingTickets (bilietai,uzsakyta);
 	    break;
 	  case 2:
 	    SoldingTickets (bilietai);
 	    break;
-	    /*
-	       case 3:
-	       Reportas (bilietai);
-	       break; */
+	   case 3:
+	       Reportas (bilietai,uzsakyta);
+	       break; 
 	  }
       }
     while (menuItem != 4);
-for (int i = 0; i < bilietai.Length; i++)
-{
-	Console.WriteLine();
-
-    Console.WriteLine("Bilietai {0}, likutis {1}",i+1,bilietai[i]);
-}
-  static int[] OrderingTickets(int[] tickets)
+  static (int[], int[]) OrderingTickets(int[] tickets, int[] ordered)
   {
     int chooseItem;
     int kiekis;
@@ -52,25 +45,26 @@ for (int i = 0; i < bilietai.Length; i++)
 	    Console.WriteLine ("Įveskite kiekį");
 	    kiekis = Convert.ToInt32 (Console.ReadLine ());
 	    tickets[0] += kiekis;
+				ordered[0] += kiekis;
 	    kiekis = 0;
 	    break;
 	  case 2:
 	    Console.WriteLine ("Įveskite kiekį");
 	    kiekis = Convert.ToInt32 (Console.ReadLine ());
 	    tickets[1] += kiekis;
-        kiekis = 0;
+				ordered[1] += kiekis;
+				kiekis = 0;
 	    break;
 	  case 3:
 	    Console.WriteLine ("Įveskite kiekį");
 	    kiekis = Convert.ToInt32 (Console.ReadLine ());
 	    tickets[2] += kiekis;
-        kiekis = 0;
+				ordered[2] += kiekis;
+				kiekis = 0;
 	    break;
 	  }
-      }
-    while (chooseItem != 4);
-
-    return tickets;
+      } while (chooseItem != 4);
+    return (tickets, ordered);
   }
   static int[] SoldingTickets (int[] tickets)
   {
@@ -127,7 +121,7 @@ for (int i = 0; i < bilietai.Length; i++)
                 }
               
                 tickets[1] -= kiekis;
-	    kiekis = 0;
+				kiekis = 0;
 	    break;
 	  case 3:
                 if (tickets[2] == 0)
@@ -148,7 +142,7 @@ for (int i = 0; i < bilietai.Length; i++)
                 }
                 
                 tickets[2] -= kiekis;
-	    kiekis = 0;
+				kiekis = 0;
 	    break;
 	  }
       }
@@ -157,7 +151,47 @@ for (int i = 0; i < bilietai.Length; i++)
     return tickets;
 
   }
-/*
-static Reportas(int[] tickets) {
-    return (int) tickets;
-}*/
+
+static (int[], int[]) Reportas(int[] tickets, int[] ordered) 
+{
+	int chooseItem;
+	 do { 
+	Console.WriteLine("A T A S K A I T A");
+	Console.WriteLine("---------------------------------");
+	Console.WriteLine("1. Užsakyta bilietų iš viso už 10€");
+	Console.WriteLine("2. Užsakyta bilietų iš viso už 20€");
+	Console.WriteLine("3. Užsakyta bilietų iš viso už 30€");
+	Console.WriteLine("4. Parduota bilietų už 10€");
+	Console.WriteLine("5. Parduota bilietų už 20€");
+	Console.WriteLine("6. Parduota bilietų už 30€");
+	Console.WriteLine("7. Likutis bilietų už 10€");
+	Console.WriteLine("8. Likutis bilietų už 20€");
+	Console.WriteLine("9. Likutis bilietų už 30€");
+	Console.WriteLine("10. Baigti ataskaitą");
+	chooseItem = Convert.ToInt32(Console.ReadLine());
+   	switch (chooseItem) 
+		{
+		case 1:
+			Console.WriteLine("Užsakyta bilietų už 10€ – {0}", ordered[0]); break;
+		case 2:
+			Console.WriteLine("Užsakyta bilietų už 20€ – {0}", ordered[1]); break;
+		case 3:
+			Console.WriteLine("Užsakyta bilietų už 30€ – {0}", ordered[2]); break;
+		case 4:
+			Console.WriteLine("Nupirkta bilietų už 10€ – {0}", ordered[0] - tickets[0]); break;
+		case 5:
+			Console.WriteLine("Nupirkta bilietų už 20€ – {0}", ordered[1] - tickets[1]); break;
+		case 6:
+			Console.WriteLine("Nupirkta bilietų už 30€ – {0}", ordered[2] - tickets[2]); break;
+		case 7:
+			Console.WriteLine("Likutis bilietų už 10€ – {0}", tickets[0]); break;
+		case 8:
+			Console.WriteLine("Likutis bilietų už 20€ – {0}", tickets[1]); break;
+		case 9:
+			Console.WriteLine("Likutis bilietų už 30€ – {0}", tickets[2]); break;
+	}
+} 
+	while (chooseItem != 10);
+	Console.WriteLine("-----------------------------------------------------------------");
+	return  (tickets, ordered);
+}
